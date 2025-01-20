81eecbee1e3e0fe79fd9cffcfe4b5380c98588a7
ritesh barman <115187468+riteshbarman02@users.noreply.github.com>
2025-01-20 17:59:59 +0530

    add search functionality to Navbar, restructure layout, and include react-icons dependency

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import "./navbar.scss";
+import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="content">
-        <div class="logo">
-          <span class="letter r">R</span>
-          <span class="letter b">B</span>
+        <div className="left-navigator">
+          <div class="logo">
+            <span class="letter r">R</span>
+            <span class="letter b">B</span>
+          </div>
+          <div className="menu">
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Skills</li>
+              <li>Blog</li>
+            </ul>
+          </div>
         </div>
-        <div className="menu">
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Blog</li>
-        </ul>
+        <div className="right-navigator">
+          <div className="searchbar">
+            <label htmlFor="search"><CiSearch /></label>
+            <input type="text" placeholder="Search..." />
+          </div>
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 };
